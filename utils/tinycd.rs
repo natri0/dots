fd830c12ee2340c5290f31872bbc8897ed1336bc
natri0 <dev@natri.fyi>
2026-02-28 22:21:42 +0200

    utils(tinycd): validate signature with public key from config
    the signature is supposed to be in `CD-Signature` header as a hex string,
    valid with the command name as the plaintext.
    e.g. for `/run/test-command` this pseudocode should return `true`:
    
        config.pubkey.verify("test-command", headers["CD-Signature"])

--- utils/tinycd.rs
+++ utils/tinycd.rs
@@ -7,14 +7,19 @@ tokio = { version = "1", features = ["rt", "macros"] }
 serde = { version = "1", features = ["derive"] }
 toml = "1"
 hex = { version = "0.4", features = ["serde"] }
+
+ed25519-dalek = { version = "2.2.0", features = ["serde"] }
 ---
 
 use std::collections::HashMap;
 use std::fs;
 use std::sync::Arc;
 
-use axum::{Router, routing::get, response::IntoResponse, http::status::StatusCode};
+use axum::{Router, routing::get, response::IntoResponse};
+use axum::http::{status::StatusCode, header::HeaderMap};
 use axum::extract::{Path, State};
+
+use ed25519_dalek::{VerifyingKey, PUBLIC_KEY_LENGTH, Signature};
 
 #[derive(serde::Deserialize)]
 #[serde(rename_all = "kebab-case")]
@@ -22,7 +27,7 @@ struct Config {
   listen_addr: String,
 
   #[serde(with = "hex")]
-  pubkey: [u8; 32],
+  pubkey: [u8; PUBLIC_KEY_LENGTH],
 
   base_dir: String,
 
@@ -63,11 +68,28 @@ async fn main() {
 
 async fn handle_run(
   Path(cmd): Path<String>,
-  State(config): State<Arc<Config>>
+  State(config): State<Arc<Config>>,
+  headers: HeaderMap
 ) -> impl IntoResponse {
   if !config.commands.contains_key(&cmd) {
     return (StatusCode::NOT_FOUND, "no such command");
   }
 
+  let Ok(pubkey) = VerifyingKey::from_bytes(&config.pubkey) else {
+    return (StatusCode::INTERNAL_SERVER_ERROR, "bad pubkey in config");
+  };
+
+  if !check_signature(&cmd, &headers, &pubkey) {
+    return (StatusCode::BAD_REQUEST, "missing or invalid signature");
+  }
+
   (StatusCode::OK, "ran successfully")
+}
+
+fn check_signature(cmd: &str, headers: &HeaderMap, pubkey: &VerifyingKey) -> bool {
+  let Some(signature) = headers.get("CD-Signature") else { return false; };
+  let Ok(signature) = hex::decode(signature) else { return false; };
+  let Ok(signature) = Signature::from_slice(&signature) else { return false; };
+
+  pubkey.verify_strict(cmd.as_bytes(), &signature).is_ok()
 }
