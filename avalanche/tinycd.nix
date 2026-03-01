9f97c3b5646e0827430b1ec45840edffc6f81344
natri0 <dev@natri.fyi>
2026-03-01 20:31:09 +0200

    add tinycd hash

--- avalanche/tinycd.nix
+++ avalanche/tinycd.nix
@@ -1,5 +1,5 @@
 { fetchzip }:
 fetchzip {
   url = "https://nightly.link/natri0/dots/workflows/build-utils/main/tinycd.zip";
-  hash = "";
+  hash = "sha256-U3P95fK55lcak1CKmKo3EHC3JrAG9Gz3oEdlwetd9Jw=";
 }
