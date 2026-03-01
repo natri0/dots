2059539b0b2785d63664c4f709fff35d7b37dd63
natri0 <dev@natri.fyi>
2026-03-01 19:57:18 +0200

    add test derivation for tinycd

---
+++ avalanche/tinycd.nix
@@ -0,0 +1,8 @@
+let
+  actionOutput = import ../utils/actions-output.nix;
+in
+actionOutput {
+  artifactName = "tinycd";
+  actionRun = "22548255794";
+  zipDerivationHash = "";
+}
---
+++ utils/action-output.nix
@@ -0,0 +1,16 @@
+{ artifactName, actionRun, zipDerivationHash }:
+let
+  repo = "natri0/dots";
+  runInfoUrl = https://api.github.com/repos/${repo}/actions/runs/${actionRun}/artifacts;
+
+  matchArtifact = name: artifact: artifact.name == name;
+  artifactJson = first builtins.filter (matchArtifact artifactName) (builtins.fromJSON builtins.readFile builtins.fetchurl runInfoUrl).artifacts;
+
+  artifact = pkgs.fetchzip {
+    url = builtins.fetchurl artifactJson.archive_download_url;
+    hash = zipDerivationHash;
+  };
+in stdenv.mkDerivation {
+  name = artifactName;
+  src = artifact;
+}
