28c76be945136386cc10769fc250e03cb35f7a6d
natri0 <dev@natri.fyi>
2026-03-01 20:34:38 +0200

    add install step for tinycd

--- avalanche/tinycd.nix
+++ avalanche/tinycd.nix
@@ -1,5 +1,16 @@
-{ fetchzip }:
-fetchzip {
-  url = "https://nightly.link/natri0/dots/workflows/build-utils/main/tinycd.zip";
-  hash = "sha256-U3P95fK55lcak1CKmKo3EHC3JrAG9Gz3oEdlwetd9Jw=";
+{ fetchzip, stdenv }:
+stdenv.mkDerivation {
+  name = "tinycd";
+
+  src = fetchzip {
+    url = "https://nightly.link/natri0/dots/workflows/build-utils/main/tinycd.zip";
+    hash = "sha256-U3P95fK55lcak1CKmKo3EHC3JrAG9Gz3oEdlwetd9Jw=";
+  };
+
+  dontBuild = true;
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src/tinycd $out/bin/tinycd
+    chmod +x $out/bin/tinycd
+  ''
 }
